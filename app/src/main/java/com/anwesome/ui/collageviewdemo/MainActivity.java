2cc6e53f13e98736fc8a173ff09fcf9a789f9f59
Anwesh43 <anweshthecool0@gmail.com>
2017-05-03 15:04:11 +0530

    Using CollageView in app

--- app/src/main/java/com/anwesome/ui/collageviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/collageviewdemo/MainActivity.java
@@ -1,13 +1,28 @@
 package com.anwesome.ui.collageviewdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.collageview.CollageUI;
 
+public class MainActivity extends AppCompatActivity {
+    private int images[] = {R.drawable.back1,R.drawable.back2,R.drawable.back3,R.drawable.back4};
+    private Bitmap bitmaps[] = new Bitmap[images.length];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CollageUI collageUI = new CollageUI(this);
+        int i = 0;
+        for(int image:images) {
+            bitmaps[i] = BitmapFactory.decodeResource(getResources(),image);
+            i++;
+        }
+        for(Bitmap bitmap:bitmaps) {
+            collageUI.addImage(bitmap);
+        }
+        collageUI.show();
     }
 }
