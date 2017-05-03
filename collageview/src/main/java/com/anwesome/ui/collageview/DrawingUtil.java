21c2568cb52a6081d7108a59fc4d507bdde58d43
Anwesh43 <anweshthecool0@gmail.com>
2017-05-03 15:30:10 +0530

    changes in drawing logic in DrawingUtil

--- collageview/src/main/java/com/anwesome/ui/collageview/DrawingUtil.java
+++ collageview/src/main/java/com/anwesome/ui/collageview/DrawingUtil.java
@@ -10,14 +10,19 @@ public class DrawingUtil {
         Path path = new Path();
         path.moveTo(0,0);
         path.lineTo(w,0);
-        int triSize = Math.min(w,h)/10;
+        int triSize = Math.min(w,h)/6;
         path.lineTo(w,h-triSize);
         path.lineTo(w-triSize,h);
         path.lineTo(0,h);
         path.lineTo(0,0);
         canvas.clipPath(path);
         canvas.drawBitmap(bitmap,0,0,paint);
-        paint.setColor(Color.parseColor("#757575"));
-        canvas.drawPath(path,paint);
+        paint.setColor(Color.parseColor("#9E9E9E"));
+        Path newPath = new Path();
+        newPath.moveTo(w-triSize,h-triSize);
+        newPath.lineTo(w-triSize,h);
+        newPath.lineTo(w,h-triSize);
+        newPath.lineTo(w-triSize,h-triSize);
+        canvas.drawPath(newPath,paint);
     }
 }
